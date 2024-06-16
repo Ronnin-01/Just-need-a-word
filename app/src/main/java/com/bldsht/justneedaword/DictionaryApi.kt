8dc2e90f44637fbe05bde3ce16bb41b34aa57a42
Ronnin-01 <omarmd425@gmail.com>
2024-06-17 00:51:21 +0530

    Retrofit call

---
+++ app/src/main/java/com/bldsht/justneedaword/DictionaryApi.kt
@@ -0,0 +1,10 @@
+package com.bldsht.justneedaword
+
+import com.bldsht.justneedaword.responsemodel.WordResult
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DictionaryApi {
+    @GET("/en/{word}")
+    suspend fun getMeaning(@Path("word") word : String) : retrofit2.Response<List<WordResult>>
+}
--- app/src/main/java/com/bldsht/justneedaword/MainActivity.kt
+++ app/src/main/java/com/bldsht/justneedaword/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.bldsht.justneedaword
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bldsht.justneedaword.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.searchBtn.setOnClickListener {
+            val word = binding.searchInput.text.toString()
+            getMeaning(word)
+        }
+
+    }
+
+    private fun getMeaning(word: String) {
+        GlobalScope.launch {
+           val response =  RetrofitCall.dictionaryApi.getMeaning(word)
+            Log.d("Word Response", response.body().toString())
+        }
 
 
     }
