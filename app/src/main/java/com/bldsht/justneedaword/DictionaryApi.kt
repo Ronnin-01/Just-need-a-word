701172eaf5bfd292b47a51c9fb3df039091be166
Ronnin-01 <omarmd425@gmail.com>
2024-06-21 19:38:24 +0530

    upgraded

--- app/src/main/java/com/bldsht/justneedaword/DictionaryApi.kt
+++ app/src/main/java/com/bldsht/justneedaword/DictionaryApi.kt
@@ -5,6 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface DictionaryApi {
+
     @GET("en/{word}")
     suspend fun getMeaning(@Path("word") word : String) : retrofit2.Response<List<WordResult>>
 }
